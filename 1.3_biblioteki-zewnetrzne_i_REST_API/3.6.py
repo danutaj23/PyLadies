ed510bb4a41f9f33ee9d66361865e50ca71198fa
danutaj23 <danoota@o2.pl>
2017-11-24 15:39:20 +0100

    update

--- 1.3_biblioteki-zewnetrzne_i_REST_API/3.6.py
+++ 1.3_biblioteki-zewnetrzne_i_REST_API/3.6.py
@@ -1,8 +1,4 @@
-# Korzystając z API http://py.net/ sprubój użyc querry stringów (query_string)
-
-# import requests
-# resp = requests.get('http://py.net/query_string')
-# print(resp.json())
+# Korzystając z API http://py.net/ spróbuj użyc querry stringów (query_string)
 
 import requests
 resp = requests.get("http://py.net/query_string?name=my_name&cokolwiek=cos")
