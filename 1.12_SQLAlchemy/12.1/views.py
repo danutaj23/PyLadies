eac8910201a38db2da68213270a7a731c9052254
danutaj23 <danoota@o2.pl>
2018-01-31 09:42:42 +0100

    update

--- 1.12_SQLAlchemy/12.1/views.py
+++ 1.12_SQLAlchemy/12.1/views.py
@@ -1,5 +1,5 @@
-from flask import render_template, request, redirect
-from main import app, db
+from flask import render_template
+from main import app
 from models import BlogPosts
 
 
@@ -7,13 +7,3 @@ from models import BlogPosts
 def info():
     blogposts = BlogPosts.query.all()
     return render_template('info.html', blogposts=blogposts)
-
-
-@app.route('/new-post', methods=['POST'])
-def new_post():
-    content = request.form['content']
-    subject = request.form['subject']
-    post = BlogPosts(text=content, subject=subject)
-    db.session.add(post)
-    db.session.commit()
-    return redirect('/')
